06d281c36409c0b084ac97ebed2c39e835d228e5
LenaMoromisato <moromisato.lena@gmail.com>
2023-04-25 15:19:58 -0300

    then e json

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-var consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/');
+var consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/').then(resposta => resposta.json()).then(r => console.log(r));
 
 console.log (consultaCEP);
