e6e0a71b0c5a7888e5a078067172ca845ece1b7e
LenaMoromisato <moromisato.lena@gmail.com>
2023-04-25 15:42:55 -0300

    tratando erros com catch

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-var consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/').then(resposta => resposta.json()).then(r => console.log(r));
+var consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/')
+.then(resposta => resposta.json())
+.then(r => {
+    if (r.erro) {
+        throw Error('Esse CEP não existe!')
+    } else
+    console.log(r)})
+.catch(erro => console.log(erro));
 
 console.log (consultaCEP);
