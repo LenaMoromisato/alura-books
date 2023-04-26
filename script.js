f44d339549d96a70b08892dbc962885222defe1e
LenaMoromisato <moromisato.lena@gmail.com>
2023-04-26 09:55:41 -0300

    async await

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
-var consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/')
-.then(resposta => resposta.json())
-.then(r => {
-    if (r.erro) {
-        throw Error('Esse CEP não existe!')
-    } else
-    console.log(r)})
-.catch(erro => console.log(erro))
-.finally(mensagem => console.log('Processamento concluído!'));
+async function buscaEndereco() {
+    var consultaCEP = await fetch('https://viacep.com.br/ws/01001000/json/');
+    var consultaCEPConvertida = await consultaCEP.json();
 
-console.log (consultaCEP);
+    console.log(consultaCEPConvertida);
+}
+
+buscaEndereco();
