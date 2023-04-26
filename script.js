b37ebb9f66c9954fb1b1982519698127609199aa
LenaMoromisato <moromisato.lena@gmail.com>
2023-04-26 10:08:27 -0300

    tratamento de erros

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 async function buscaEndereco() {
+    try {
     var consultaCEP = await fetch('https://viacep.com.br/ws/01001000/json/');
     var consultaCEPConvertida = await consultaCEP.json();
 
+    if(consultaCEPConvertida.erro) {
+        throw Error('CEP não existente!');
+    }
     console.log(consultaCEPConvertida);
+    } catch (erro) {
+        console.log(erro);
+    }
 }
 
 buscaEndereco();
