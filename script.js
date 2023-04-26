190fd7facd3665b74c7a4ebc80045c56a5abf6b9
LenaMoromisato <moromisato.lena@gmail.com>
2023-04-26 15:04:25 -0300

    desafio input bairro

--- script.js
+++ script.js
@@ -11,10 +11,12 @@ async function buscaEndereco(cep) {
         var cidade = document.getElementById('cidade');
         var logradouro = document.getElementById('endereco');
         var estado = document.getElementById('estado');
+        var bairro = document.getElementById('bairro');
 
         cidade.value = consultaCEPConvertida.localidade;
         logradouro.value = consultaCEPConvertida.logradouro;
         estado.value = consultaCEPConvertida.uf;
+        bairro.value = consultaCEPConvertida.bairro;
 
         console.log(consultaCEPConvertida);
         return consultaCEPConvertida;
