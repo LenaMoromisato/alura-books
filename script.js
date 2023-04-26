3bc9b09ff1aa1d56e0c16b685c9fba6a9f8dcd63
LenaMoromisato <moromisato.lena@gmail.com>
2023-04-26 10:22:17 -0300

    promise all

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
-async function buscaEndereco() {
+async function buscaEndereco(cep) {
     try {
-    var consultaCEP = await fetch('https://viacep.com.br/ws/01001000/json/');
-    var consultaCEPConvertida = await consultaCEP.json();
+        var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+        var consultaCEPConvertida = await consultaCEP.json();
 
-    if(consultaCEPConvertida.erro) {
-        throw Error('CEP não existente!');
-    }
-    console.log(consultaCEPConvertida);
+        if(consultaCEPConvertida.erro) {
+            throw Error('CEP não existente!');
+        }
+        console.log(consultaCEPConvertida);
+        return consultaCEPConvertida;
     } catch (erro) {
         console.log(erro);
     }
 }
 
-buscaEndereco();
+let ceps = ['01001000','01001001'];
+let conjuntoCeps = ceps.map(valores => buscaEndereco(valores));
+
+Promise.all(conjuntoCeps).then(respostas => console.log(respostas));
