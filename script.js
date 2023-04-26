c42796f08b1ca26c94826515befabc1bde193d04
LenaMoromisato <moromisato.lena@gmail.com>
2023-04-26 15:00:14 -0300

    evitando problemas

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 async function buscaEndereco(cep) {
+    var mensagemErro = document.getElementById('erro');
+    mensagemErro.innerHTML = "";
     try {
         var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
         var consultaCEPConvertida = await consultaCEP.json();
@@ -17,6 +19,7 @@ async function buscaEndereco(cep) {
         console.log(consultaCEPConvertida);
         return consultaCEPConvertida;
     } catch (erro) {
+        mensagemErro.innerHTML = `<p>CEP inválido. Tente novamente!</p>`;
         console.log(erro);
     }
 }
